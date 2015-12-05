e01965237bf61377e9e9a2ff02c610cffb088376
Thondar <jonasbilljensen@gmail.com>
2015-12-05 02:18:51 +0100

    Initial Setup

---
+++ src/main/java/com/tt/towncraft/TownCraft.java
@@ -0,0 +1,29 @@
+package com.tt.towncraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="towncraft", name="TownCraft", version="1.7.10-1.0")
+public class TownCraft {
+
+    @Mod.Instance("TownCraft")
+    public static TownCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPostInitializationEvent event){
+
+    }
+
+}
